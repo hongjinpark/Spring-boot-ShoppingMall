efeec1b7f178f2f2bbb89e75f6325314acc14cde
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-10-05 23:37:11 +0900

    OR 조건 쿼리 메소드 이름 수정

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -39,7 +39,7 @@ class ItemRepositoryTest {
     }
 
     @Test
-    @DisplayName("상품명 조회 테스트")
+    @DisplayName("상품명, 상품상세설명 or 테스트")
     public void findByItemNmTest() {
         this.createItemList();
         List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명 5");
