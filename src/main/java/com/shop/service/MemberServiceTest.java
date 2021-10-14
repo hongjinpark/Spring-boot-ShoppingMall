1eeb087f28b112b8304abd1c93a7acfb59126410
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-10-14 22:09:50 +0900

    중복 회원 가입 테스트

--- src/main/java/com/shop/service/MemberServiceTest.java
+++ src/main/java/com/shop/service/MemberServiceTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.TestPropertySource;
 import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -50,5 +51,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
         assertEquals(member.getRole(), savedMember.getRole());
     }
 
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateMemberTest() {
+
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);});
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
 
 }
