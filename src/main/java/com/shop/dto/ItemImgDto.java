7e6f1c42b75680d85e09d879a0586da73545b99e
ektkfk180 <81549627+ektkfk180@users.noreply.github.com>
2021-11-05 21:31:00 +0900

    ItemImgDto 코드 수정
    id 추가

--- src/main/java/com/shop/dto/ItemImgDto.java
+++ src/main/java/com/shop/dto/ItemImgDto.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 @Getter @Setter
 public class ItemImgDto {
 
+    private Long id;
     private String imgName;
     private String oriImgName;
     private String imgUrl;
